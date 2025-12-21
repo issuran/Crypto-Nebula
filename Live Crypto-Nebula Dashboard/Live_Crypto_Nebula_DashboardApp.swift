4aa9e47828ff887a836c51a790f4b8b3797d3199
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2025-12-21 18:20:41 -0300

    Initial Commit

---
+++ Live Crypto-Nebula Dashboard/Live_Crypto_Nebula_DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Live_Crypto_Nebula_DashboardApp.swift
+//  Live Crypto-Nebula Dashboard
+//
+//  Created by Tiago Oliveira on 21/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Live_Crypto_Nebula_DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
